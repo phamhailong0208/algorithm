a9a3e6332f4d2ed24fe27e132af2c2ba17e1d7f3
LongPhamHai357 <dotrucbk@gmail.com>
2026-04-15 10:46:09 +0700

    N/A (0%) N/A (0%) 0088-merge-sorted-array .cpp

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         vector<int> merged(m+n);
-        int i=0,j=0,k=0;
+        int i=j=k=0;
         while ( i < m && j < n ){
             if (nums1[i] < nums2[j]) {
                 merged[k++] = nums1[i++];
