7909b9d721b22e60622ced3e59b68ef0590ec0be
LongPhamHai357 <dotrucbk@gmail.com>
2026-04-18 18:34:10 +0700

    N/A (0%) N/A (0%) 0733-flood-fill .cpp

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -3,20 +3,19 @@ public:
     void dfs(vector<vector<int>>& image, int sr, int sc, int original_color, int color) {
         int row = image.size();
         int col = image[0].size();
-        if ( sr < 0 || sr >= row || sc < 0 || sc >= col ){
-            return;
-        }
+        if ( sr <0 || sr >= row || sc <0 || sc >= col) return;
         if (image[sr][sc] != original_color) return;
         image[sr][sc] = color;
-        dfs( image,sr+1,sc, original_color, color);
-        dfs( image,sr-1,sc, original_color, color);
-        dfs( image,sr,sc+1, original_color, color);
-        dfs( image,sr,sc-1, original_color, color);
+        dfs(image, sr-1, sc, original_color, color);
+        dfs(image, sr+1, sc, original_color, color);
+        dfs(image, sr, sc-1, original_color, color);
+        dfs(image, sr, sc+1, original_color, color);
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int original_color = image[sr][sc];
         if (original_color == color) return image;
-        dfs(image,sr,sc, original_color, color);
+        dfs(image, sr,sc, orignal_color, color);
         return image;
+
     }
 };
