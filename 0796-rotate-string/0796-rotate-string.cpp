d32905a9ff6325c02559ca7f36def770e9a84679
Pham Hai Long <dotrucbk@gmail.com>
2026-05-09 16:52:43 +0700

    Time: 0 ms (100%), Space: 8.2 MB (35.46%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,8 +1,22 @@
 class Solution {
 public:
+    bool findString(string doubled, string goal) {
+        int n = doubled.length();
+        int m = goal.length();
+        if (m > n) return false;
+        for ( int i = 0; i < n-m; i++) {
+            int j=0;
+            while (j < m && doubled[i+j] == goal[j]) {
+                j++;
+            }
+            if (j == m) return true;
+        }
+        return false;
+    }
     bool rotateString(string s, string goal) {
         if (s.size() != goal.size()) return false;
         string doubled = s+s;
-        return doubled.find(goal) != string::npos;
+        return findString(doubled, goal);
+        // return doubled.find(goal) != string::npos;
     }
 };
